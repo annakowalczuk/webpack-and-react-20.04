03ab8635881dec8156cf1ee2184388fc6a447d61
Anna Kowalczuk <anna@kowalczuk.eu>
2018-11-03 16:14:39 +0100

    Refactor TodoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,15 +2,27 @@
 import React from 'react';
 import style from '../styles/TodoForm.css';
 
-const TodoForm = ({addTodo}) => {
-  let textareaValue = "";
-  return (
-    <form className={style.TodoForm} onSubmit={(e) => {e.preventDefault(); if (textareaValue!=""){addTodo(textareaValue)};}}>
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+  
+  render(){
+    return (
+    <form className={style.TodoForm} onSubmit={(e) => {
+        e.preventDefault(); 
+        if (this.state.value != ''){
+          this.props.addTodo(this.state.value)
+          };
+        this.state.value = '';
+        }}>
       <label htmlFor="todoText">Add To-do:</label>
-      <textarea id="todoText" onChange={(e) => textareaValue = event.target.value}></textarea>
+      <textarea id="todoText" value={this.state.value} onChange={(e) => this.setState({value: event.target.value})} ></textarea>
       <input type="submit" value="Submit" />
     </form>
-  );
+    );
+  }
 }
 
 export default TodoForm;
